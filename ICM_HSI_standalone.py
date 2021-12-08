988355c8eef69f9960a2bf24919180ad0666237f
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-12-07 18:12:23 -0600

    Update ICM_HSI_standalone.py

--- ICM_HSI_standalone.py
+++ ICM_HSI_standalone.py
@@ -109,81 +109,104 @@ inputs = np.genfromtxt('ICM_control.csv',dtype=str,comments='#',delimiter=',')
 # Parent directory locations for various ICM components
 # These directories must exist in the model folder
 # Other directories are created throughout the ICM run - but they are all based on these parent directories
-ecohydro_dir = os.path.normpath(inputs[1,1].lstrip().rstrip())
-wetland_morph_dir = os.path.normpath(inputs[2,1].lstrip().rstrip())
-vegetation_dir = os.path.normpath(inputs[3,1].lstrip().rstrip())
-bimode_dir = os.path.normpath(inputs[4,1].lstrip().rstrip())
-HSI_dir = os.path.normpath(inputs[5,1].lstrip().rstrip())
-ewe_dir = os.path.normpath(inputs[6,1].lstrip().rstrip())
+ecohydro_dir = os.path.normpath('%s/%s' % (par_dir,inputs[1,1].lstrip().rstrip()))
+wetland_morph_dir = os.path.normpath('%s/%s' % (par_dir,inputs[2,1].lstrip().rstrip()))
+vegetation_dir = os.path.normpath('%s/%s' % (par_dir,inputs[3,1].lstrip().rstrip()))
+bimode_dir = os.path.normpath('%s/%s' % (par_dir,inputs[4,1].lstrip().rstrip()))
+HSI_dir = os.path.normpath('%s/%s' % (par_dir,inputs[5,1].lstrip().rstrip()))
+ewe_dir = os.path.normpath('%s/%s' % (par_dir,inputs[6,1].lstrip().rstrip()))
+
+#default# hydro_exe_path = './hydro_v23.4.0' 
+#default# bidem_exe_path = './bidem_v23.0.0'
+#default# morph_exe_path = './morph_v23.1.0' 
+
+hydro_exe_path = inputs[7,1].lstrip().rstrip() # path to hydro executable - ** path is relative to the S##/G###/hydro        ** - use './' if running copy of executable that is saved in /hydro directory 
+bidem_exe_path = inputs[8,1].lstrip().rstrip() # path to bidem executable - ** path is relative to the S##/G###/bidem/REGION ** - use './' if running copy of executable that is saved in /bidem/REGION directory
+morph_exe_path = inputs[9,1].lstrip().rstrip() # path to morph executable - ** path is relative to the S##/G###              ** - use './' if running copy of executable that is saved in /G## directory
+
 # Configuration files used by various ICM components
-VegConfigFile = inputs[7,1].lstrip().rstrip()
-WMConfigFile = inputs[8,1].lstrip().rstrip()
-EHConfigFile = inputs[9,1].lstrip().rstrip()
-EHCellsFile = inputs[10,1].lstrip().rstrip()
-EHLinksFile = inputs[11,1].lstrip().rstrip()
-BIPrismFile = inputs[12,1].lstrip().rstrip()
-BIMHWFile = inputs[13,1].lstrip().rstrip()
-EHInterfaceFile = inputs[14,1].lstrip().rstrip()
-BMInterfaceFile = inputs[15,1].lstrip().rstrip()
+VegConfigFile = inputs[10,1].lstrip().rstrip()
+WMConfigFile = inputs[11,1].lstrip().rstrip()
+EHConfigFile = inputs[12,1].lstrip().rstrip()
+EHCellsFile = inputs[13,1].lstrip().rstrip()
+EHLinksFile = inputs[14,1].lstrip().rstrip()
+BIMHWFile = inputs[15,1].lstrip().rstrip()
+exist_cond_tag = inputs[16,1].lstrip().rstrip()
+fwoa_init_cond_tag = inputs[17,1].lstrip().rstrip()
+shallow_subsidence_column = int(inputs[18,1].lstrip().rstrip())
+
 compartment_output_file = 'compartment_out.csv'
-grid_output_file = 'grid_500m_out.csv'
+grid_output_file        = 'grid_500m_out.csv'
+EHInterfaceFile         = 'ICM_info_into_EH.txt'
+
 
 # Filenames for Veg model input
-WaveAmplitudeFile = inputs[16,1].lstrip().rstrip()
-MeanSalinityFile = inputs[17,1].lstrip().rstrip()
-SummerMeanWaterDepthFile = inputs[18,1].lstrip().rstrip()
-SummerMeanSalinityFile = inputs[19,1].lstrip().rstrip()
-SummerMeanTempFile = inputs[20,1].lstrip().rstrip()
-TreeEstCondFile = inputs[21,1].lstrip().rstrip()
-HtAbvWaterFile = inputs[22,1].lstrip().rstrip()
-PerLandFile = inputs[23,1].lstrip().rstrip()
+WaveAmplitudeFile = inputs[19,1].lstrip().rstrip()
+MeanSalinityFile = inputs[20,1].lstrip().rstrip()
+SummerMeanWaterDepthFile = inputs[21,1].lstrip().rstrip()
+SummerMeanSalinityFile = inputs[22,1].lstrip().rstrip()
+SummerMeanTempFile = inputs[23,1].lstrip().rstrip()
+TreeEstCondFile = inputs[24,1].lstrip().rstrip()
+HtAbvWaterFile = inputs[25,1].lstrip().rstrip()
+PerLandFile = inputs[26,1].lstrip().rstrip()
+PerWaterFile = inputs[27,1].lstrip().rstrip()
+AcuteSalFile = inputs[28,1].lstrip().rstrip()
+acute_sal_threshold = 5.5
 
 ## Simulation Settings
-startyear = int(inputs[24,1].lstrip().rstrip())
-endyear = int(inputs[25,1].lstrip().rstrip())
-nvegtype = int(inputs[26,1].lstrip().rstrip())
-inputStartYear = int(inputs[27,1].lstrip().rstrip())
-hotstart_year = int(inputs[28,1].lstrip().rstrip())
+startyear = int(inputs[29,1].lstrip().rstrip())
+endyear = int(inputs[30,1].lstrip().rstrip())
+ncycle = int(inputs[31,1].lstrip().rstrip())
+startyear_cycle = int(inputs[32,1].lstrip().rstrip())
+endyear_cycle = int(inputs[33,1].lstrip().rstrip())
+inputStartYear = int(inputs[34,1].lstrip().rstrip())
+nvegtype = int(inputs[35,1].lstrip().rstrip())
+update_hydro_attr = int(inputs[36,1].lstrip().rstrip())
+
+# convert calendar years to elapsed years
+hotstart_year = startyear_cycle
 elapsed_hotstart = hotstart_year - startyear
-update_hydro_attr = int(inputs[29,1].lstrip().rstrip())
+cycle_start_elapsed = startyear_cycle - startyear
+cycle_end_elapsed = endyear_cycle - startyear + 1
+
+hotstart_year = startyear_cycle
+cycle_start_elapsed = startyear_cycle - startyear + 1
+cycle_end_elapsed = endyear_cycle - startyear + 1
+
 
 ## grid information for Veg ASCII grid files
-n500grid= int(inputs[30,1].lstrip().rstrip())
+n500grid= int(inputs[37,1].lstrip().rstrip())
 # n500gridveg = int(inputs[25,1].lstrip().rstrip()) #total number of grid cells in Veg model - including NoData cells
-n500rows = int(inputs[31,1].lstrip().rstrip())
-n500cols = int(inputs[32,1].lstrip().rstrip())
-xll500 = int(inputs[33,1].lstrip().rstrip())
-yll500 = int(inputs[34,1].lstrip().rstrip())
+n500rows = int(inputs[38,1].lstrip().rstrip())
+n500cols = int(inputs[39,1].lstrip().rstrip())
+xll500 = int(inputs[40,1].lstrip().rstrip())
+yll500 = int(inputs[41,1].lstrip().rstrip())
 
 ## grid information for EwE ASCII grid files
-n1000grid = int(inputs[35,1].lstrip().rstrip())
-n1000rows = int(inputs[36,1].lstrip().rstrip())
-n1000cols = int(inputs[37,1].lstrip().rstrip())
-xll1000 = inputs[38,1].lstrip().rstrip()
-yll1000 = inputs[39,1].lstrip().rstrip()
+n1000grid = int(inputs[42,1].lstrip().rstrip())
+n1000rows = int(inputs[43,1].lstrip().rstrip())
+n1000cols = int(inputs[44,1].lstrip().rstrip())
+xll1000 = inputs[45,1].lstrip().rstrip()
+yll1000 = inputs[46,1].lstrip().rstrip()
 
-# file naming settings
-mpterm = inputs[40,1].lstrip().rstrip()
-sterm = inputs[41,1].lstrip().rstrip()
-gterm = inputs[42,1].lstrip().rstrip()
-cterm = inputs[43,1].lstrip().rstrip()
-uterm = inputs[44,1].lstrip().rstrip()
-vterm = inputs[45,1].lstrip().rstrip()
-rterm = inputs[46,1].lstrip().rstrip()
+# file naming convention settings
+mpterm = inputs[47,1].lstrip().rstrip()
+sterm = inputs[48,1].lstrip().rstrip()
+gterm = inputs[49,1].lstrip().rstrip()
+cterm = inputs[50,1].lstrip().rstrip()
+uterm = inputs[51,1].lstrip().rstrip()
+vterm = inputs[52,1].lstrip().rstrip()
+rterm = inputs[53,1].lstrip().rstrip()
 runprefix = '%s_%s_%s_%s_%s_%s_%s' % (mpterm,sterm,gterm,cterm,uterm,vterm,rterm)
 
 EHtemp_path = os.path.normpath(r'%s/TempFiles' % ecohydro_dir)
-
-## read Wetland Morph parameters csv file into array (first column is descriptor, second column is variable)                            
-WMConfigFilepath = os.path.normpath(r'%s/%s' % (wetland_morph_dir,WMConfigFile) )
-WM_params = np.genfromtxt(WMConfigFilepath,dtype=str,delimiter=',',usecols=1)   
 
 # read in grid-to-compartment lookup table into a dictionary
 # key is grid ID and value is compartment
 grid_lookup_file = r'%s/grid_lookup_500m.csv' % ecohydro_dir
 grid_lookup = np.genfromtxt(grid_lookup_file,skip_header=1,delimiter=',',dtype='int',usecols=[0,1])
 grid_comp = {row[0]:row[1] for row in grid_lookup}
-grid500_res = 500.0
+grid500_res = 480.0
 
 # Save list of GridIDs into an array for use in some loops later # ultimately can replace with grid_comp.keys() in loops
 gridIDs=grid_comp.keys()
@@ -272,7 +295,7 @@ for year in range(startyear,endyear+1):
     comp_summary_dict = compout2dict(comp_summary_file,10)
     OWseddep_mass_dict = comp2grid(comp_summary_dict,grid_comp)
     OWseddep_depth_mm_dict = {}
-    BDWaterVal = float(WM_params[31].lstrip().rstrip())
+    BDWaterVal = 0.835              #ow_bd - bulk density of water bottoms (g/cm3)\n")
     # convert sediment deposition loading (kg/m2) to depth (mm) using bulk density of open water area (kg/m3)
     # if deposition is negative, that indicates erosion
     
@@ -425,7 +448,7 @@ for year in range(startyear,endyear+1):
     
     # remove try/except so errors in HSI are returned and timestepping stops
     #try:
-    HSI.HSI(gridIDs,stagedict,stgmndict,bedelevdict,melevdict,saldict,tmpdict,veg_output_filepath,nvegtype,landdict,waterdict,pctsanddict,OWseddep_depth_mm_dict,pctedgedict,cultchdict,n500grid,n500rows,n500cols,yll500,xll500,year,elapsedyear,HSI_dir,WM_params,vegetation_dir,wetland_morph_dir,runprefix)
+    HSI.HSI(gridIDs,stagedict,stgmndict,bedelevdict,melevdict,saldict,tmpdict,veg_output_filepath,nvegtype,landdict,waterdict,pctsanddict,OWseddep_depth_mm_dict,pctedgedict,cultchdict,n500grid,n500rows,n500cols,yll500,xll500,year,elapsedyear,HSI_dir,vegetation_dir,wetland_morph_dir,runprefix)
     #except:
     #    print('******ERROR******')
     #    print('\n HSI model run failed - Year %s.' % year)
